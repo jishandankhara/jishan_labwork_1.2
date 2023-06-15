9c09aa773573529950ad2e2486743708221b180a
jishan dankhara <parthbhadani8467@gmail.com>
2023-06-15 17:44:04 +0530

    done

---
+++ -JISHAN2.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+int b,h,area
+clrscr();
+printf("enter b =");
+scanf("%d",&b);
+printf("enter h =");
+scanf("%d",&h);
+area=(b*h)/2;
+printf("area of trisngel =%d",area);
+getch()}
---
+++ JISHAN-2.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+int width,height,area;
+
+clrscr();
+printf("width of rectangel :");
+scanf("%d",&width);
+printf("height of rectangel :");
+scanf("%d",&height);
+area=width*height;
+printf("area of rectangel is %d",area)
+getch();
+}
